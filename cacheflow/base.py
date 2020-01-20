d847ef836e1ad2c5d4595f6362cce41343c8e999
Remi Rampin <r@remirampin.com>
2020-01-20 16:10:35 -0500

    Reorder base.py

--- cacheflow/base.py
+++ cacheflow/base.py
@@ -3,6 +3,41 @@ from hashlib import sha256
 from .cache.core import UNHASHABLE, hash_value
 
 
+class StepInputConnection(object):
+    def __init__(self, source_step_id, source_output_name):
+        self.source_step_id = source_step_id
+        self.source_output_name = source_output_name
+
+    def __repr__(self):
+        return '%s.%s' % (self.source_step_id, self.source_output_name)
+
+
+class Step(object):
+    def __init__(self, id, component_def, inputs):
+        self.id = id
+        self.component_def = component_def
+        self.inputs = inputs
+
+    def __repr__(self):
+        return '<Step {!r} type={!r} inputs={{{}}}>'.format(
+            self.id,
+            self.component_def.get('type'),
+            ', '.join('%s=%r' % p for p in sorted(self.inputs.items())),
+        )
+
+
+class Workflow(object):
+    def __init__(self, steps, meta):
+        self.steps = steps
+        self.meta = meta
+
+    def __repr__(self):
+        return '<Workflow\n  steps:{}\n  meta={!r}>'.format(
+            ''.join('\n    {!r}'.format(s) for s in self.steps.values()),
+            self.meta,
+        )
+
+
 class Component(object):
     """A workflow component, as provided by a `ComponentLoader`.
     """
@@ -38,38 +73,3 @@ class ComponentLoader(object):
         """Returns a component or None.
         """
         raise NotImplementedError
-
-
-class Workflow(object):
-    def __init__(self, steps, meta):
-        self.steps = steps
-        self.meta = meta
-
-    def __repr__(self):
-        return '<Workflow\n  steps:{}\n  meta={!r}>'.format(
-            ''.join('\n    {!r}'.format(s) for s in self.steps.values()),
-            self.meta,
-        )
-
-
-class StepInputConnection(object):
-    def __init__(self, source_step_id, source_output_name):
-        self.source_step_id = source_step_id
-        self.source_output_name = source_output_name
-
-    def __repr__(self):
-        return '%s.%s' % (self.source_step_id, self.source_output_name)
-
-
-class Step(object):
-    def __init__(self, id, component_def, inputs):
-        self.id = id
-        self.component_def = component_def
-        self.inputs = inputs
-
-    def __repr__(self):
-        return '<Step {!r} type={!r} inputs={{{}}}>'.format(
-            self.id,
-            self.component_def.get('type'),
-            ', '.join('%s=%r' % p for p in sorted(self.inputs.items())),
-        )
