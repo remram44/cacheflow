6b183633f937faa6bd88ede7ee764a2c90c5df00
Remi Rampin <r@remirampin.com>
2020-01-26 18:31:27 -0500

    Move websocket endpoint to new api.py

---
+++ cacheflow/web/api.py
@@ -0,0 +1,75 @@
+import json
+import logging
+from tornado.websocket import WebSocketHandler
+
+from ..storage.controller import WorkflowChangeObserver
+from .json import workflow_to_json, json_to_actions, action_to_json
+
+
+logger = logging.getLogger(__name__)
+
+
+class WorkflowWS(WebSocketHandler, WorkflowChangeObserver):
+    def open(self):
+        self.application.controller.add_change_observer(self)
+        logger.info("WebSocket connected")
+
+        # Send components library
+        components = []
+        for loader in self.application.controller.executor.component_loaders:
+            for info, component_def in loader.list_components():
+                comp_dict = dict(info, component_def=component_def)
+                comp_dict.setdefault('inputs', [])
+                comp_dict.setdefault('outputs', [])
+                components.append(comp_dict)
+        self.write_message({
+            'type': 'components_add',
+            'components': components,
+        })
+
+        # Send current workflow
+        self.write_message({
+            'type': 'workflow',
+            'workflow': workflow_to_json(
+                self.application.controller,
+            ),
+        })
+
+    # DEBUG
+    def write_message(self, message):
+        logger.info("< %s", message)
+        super(WorkflowWS, self).write_message(message)
+
+    def on_message(self, message):
+        logger.info("> %s", message)  # DEBUG
+        message = json.loads(message)
+        type_ = message.pop('type')
+        if type_.startswith('workflow_'):
+            actions_array = json_to_actions(
+                type_, message,
+                self.application.controller,
+            )
+            for action in actions_array:
+                self.application.controller.apply_action(action)
+        else:
+            logger.error("Got invalid message %r", type_)
+
+    def on_close(self):
+        self.application.controller.remove_change_observer(self)
+        logger.info("WebSocket disconnected")
+
+    def check_origin(self, origin):
+        return True
+
+    def on_workflow_action(self, action):
+        # TODO: Send incremental updates
+        if False:
+            message = action_to_json(action, self.application.controller)
+        else:
+            message = {
+                'type': 'workflow',
+                'workflow': workflow_to_json(
+                    self.application.controller,
+                ),
+            }
+        self.write_message(message)
--- cacheflow/web/main.py
+++ cacheflow/web/main.py
@@ -1,21 +1,19 @@
 import argparse
-import json
 import logging
 import pkg_resources
 import tornado.ioloop
 import tornado.iostream
 from tornado.routing import URLSpec
 import tornado.web
-from tornado.websocket import WebSocketHandler
 import webbrowser
 
 from .. import __version__
 from ..base import Step, StepInputConnection, Workflow
 from ..cache import NullCache
 from ..executor import Executor
-from ..storage.controller import WorkflowChangeObserver, WorkflowController
+from ..storage.controller import WorkflowController
 from .base import BaseHandler
-from .json import workflow_to_json, json_to_actions, action_to_json
+from . import api
 
 
 logger = logging.getLogger(__name__)
@@ -24,8 +22,6 @@ logger = logging.getLogger(__name__)
 class Application(tornado.web.Application):
     def __init__(self, handlers, **kwargs):
         super(Application, self).__init__(handlers, **kwargs)
-
-        self.client_sockets = set()
 
         executor = Executor(NullCache())
         executor.add_components_from_entrypoint()
@@ -79,72 +75,6 @@ class Application(tornado.web.Application):
         self.controller = WorkflowController(workflow, executor)
 
 
-class WorkflowWS(WebSocketHandler, WorkflowChangeObserver):
-    def open(self):
-        self.application.controller.add_change_observer(self)
-        logger.info("WebSocket connected")
-
-        # Send components library
-        components = []
-        for loader in self.application.controller.executor.component_loaders:
-            for info, component_def in loader.list_components():
-                comp_dict = dict(info, component_def=component_def)
-                comp_dict.setdefault('inputs', [])
-                comp_dict.setdefault('outputs', [])
-                components.append(comp_dict)
-        self.write_message({
-            'type': 'components_add',
-            'components': components,
-        })
-
-        # Send current workflow
-        self.write_message({
-            'type': 'workflow',
-            'workflow': workflow_to_json(
-                self.application.controller,
-            ),
-        })
-
-    # DEBUG
-    def write_message(self, message):
-        logger.info("< %s", message)
-        super(WorkflowWS, self).write_message(message)
-
-    def on_message(self, message):
-        logger.info("> %s", message)  # DEBUG
-        message = json.loads(message)
-        type_ = message.pop('type')
-        if type_.startswith('workflow_'):
-            actions_array = json_to_actions(
-                type_, message,
-                self.application.controller,
-            )
-            for action in actions_array:
-                self.application.controller.apply_action(action)
-        else:
-            logger.error("Got invalid message %r", type_)
-
-    def on_close(self):
-        self.application.controller.remove_change_observer(self)
-        logger.info("WebSocket disconnected")
-
-    def check_origin(self, origin):
-        return True
-
-    def on_workflow_action(self, action):
-        # TODO: Send incremental updates
-        if False:
-            message = action_to_json(action, self.application.controller)
-        else:
-            message = {
-                'type': 'workflow',
-                'workflow': workflow_to_json(
-                    self.application.controller,
-                ),
-            }
-        self.write_message(message)
-
-
 class Index(BaseHandler):
     def get(self):
         return self.redirect(self.reverse_url('workflow_view', 'default'))
@@ -177,7 +107,7 @@ def make_app(debug=False):
                 '/static/(.*)', tornado.web.StaticFileHandler,
                 {'path': static_dir},
             ),
-            URLSpec('/workflow', WorkflowWS),
+            URLSpec('/workflow', api.WorkflowWS),
         ],
         # TODO: Security settings (CSRF, cookie, single-user auth token)
         xsrf_cookies=False,
