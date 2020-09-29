e10aa02d4f3b5f408e6df86a5fd9b6d482a9f187
Remi Rampin <r@remirampin.com>
2020-09-29 13:52:10 -0400

    WIP

---
+++ ui/src/components/Connection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Connection.css';
+
+interface ConnectionProps {}
+
+export class Connection extends React.PureComponent<ConnectionProps> {
+  render() {
+    // TODO: Connection svg
+    return <></>;
+  }
+}
---
+++ ui/src/components/Port.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Port.css';
+
+interface PortProps {
+  position: [number, number];
+}
+
+export class Port extends React.PureComponent<PortProps> {
+  render() {
+    // TODO: Port svg
+    return <></>;
+  }
+}
